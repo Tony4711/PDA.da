769e8c688316d60d90cadf744863d213a4ba87fa
Tony4711 <tony.schultze96@gmail.com>
2025-10-18 21:12:00 +0200

    Refactored: modularize user creation (name, diagnosis, style, triggers)
    Added helper methods get_user_name(), get_diagnosis(), get_style(),  and get_trigger_words(). create_new_user() refactored to use modular getters instead of inline input code.

--- src/chatbot.py
+++ src/chatbot.py
@@ -10,20 +10,33 @@ class ChatBot():
     def load_api_key(self):
         load_dotenv()
         client = OpenAI(api_key=os.getenv("OpenAI_API_KEY"))
-          
+
+    def get_user_name(self):
+        user_name = PDA_Utility.read_raw_input()
+        return user_name
+    
+    def get_diagnosis(self):
+        valid_diagnosis = {}
+        diagnosis_raw = PDA_Utility.read_raw_input()
+        diagnosis = PDA_Utility.chop_input(diagnosis_raw)
+        for d in diagnosis:
+             valid_diagnosis[d] = PDA_Utility.get_valid_diagnosis(d)
+        return valid_diagnosis
+    
+    def get_style(self):
+         pass
+    
+    def get_trigger_words(self):
+         pass
+
     def talk_loop(self):
             self.answers = {}
             for q in self.questions:
                 PDA_Utility.print_input(Chatter.BOT.value, q)
                 user_txt = self.get_input()
-                self.answers[q] = user_txt
-            
+                self.answers[q] = user_txt    
+    
     def create_new_user(self):
-        user_name = input(">> ")
-        diagnosis = {}
-        key = self.read_input()
-        while key != ('\r', '\n'):
-            for i in (range(len(Diagnosis))):
-                diagnosis[i] = input()
-        user = UserProfile(user_name) 
-                           #[Diagnosis.ADHS, Diagnosis.ASS, Diagnosis.PDA], [Style.PANDA, Style.HONEST, Style.DIRECT])
+        user = UserProfile(self.get_user_name(), self.get_diagnosis(), self.get_style(), self.get_trigger_words())     
+        print(user)
+    
