e3766715655d75f6606f2cce9e0102194d077da6
Tony4711 <tony.schultze96@gmail.com>
2025-10-18 14:40:35 +0200

    Init: first push of enums.py

---
+++ src/enums.py
@@ -0,0 +1,21 @@
+from enum import Enum, auto
+
+class Chatter(Enum):
+
+    BOT = "Bot"
+    USER = "User"
+
+class Diagnosis(Enum):
+
+    PDA = "PDA"
+    ADHS = "ADHS"
+    ASS = "Autismus-Spektrum-Störung"
+
+class Style(Enum):
+
+    CALM = "ruhig"
+    PANDA = "Kämpfe weise auswählen, Angstmanagement, Verhandlung & Zusammenarbeit, Anforderungen verschleiern & managen, Anpassung"
+    DIRECT = "direkt"
+    EMPATHIC = "emphatisch"
+    HONEST = "ehrlich"
+
